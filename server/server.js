b6a93c657aaadb1d840064d235ff3ac642126540
Patric Jansson <paddy@kth.se>
2018-01-11 15:32:11 +0100

    Only load .env on nodeEnv dev

--- server/server.js
+++ server/server.js
@@ -3,14 +3,10 @@ const server = require('kth-node-server')
 const path = require('path')
 // Load .env file in development mode
 const nodeEnv = process.env.NODE_ENV && process.env.NODE_ENV.toLowerCase()
-if (nodeEnv === 'development' || nodeEnv === 'dev' || !nodeEnv) {
+if (nodeEnv === 'development' || nodeEnv === 'dev') {
   require('dotenv').config()
-} else if (!process.env.SERVICE_PUBLISH) {
-  // This is an ANSIBLE machine which doesn't set env-vars atm
-  // so read localSettings.js which we now use to fake env-vars
-  // because it already exists in our Ansible setup.
-  require('../config/localSettings')
 }
+
 // Now read the server config etc.
 const config = require('./configuration').server
 const AppRouter = require('kth-node-express-routing').PageRouter
