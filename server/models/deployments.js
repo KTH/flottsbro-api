20b7c500f7ea1c474711cad0fe5f572f47a03963
Patric Jansson <paddy@kth.se>
2018-03-12 17:00:12 +0100

    standardjs

--- server/models/deployments.js
+++ server/models/deployments.js
@@ -1,84 +1,85 @@
 'use strict'
+
 const mongoose = require('mongoose')
 
 const schema = mongoose.Schema(
   {
-    "created": Number,
-    "service_file_md5": String,
-    "application_name": String,
-    "cluster": {
-      "environment": [
+    'created': Number,
+    'service_file_md5': String,
+    'application_name': String,
+    'cluster': {
+      'environment': [
         {
-          "value": String,
-          "key": String
+          'value': String,
+          'key': String
         },
         {
-          "value": String,
-          "key": String
+          'value': String,
+          'key': String
         },
         {
-          "value": String,
-          "key": String
+          'value': String,
+          'key': String
         },
         {
-          "value": String,
-          "key": String
+          'value': String,
+          'key': String
         },
         {
-          "value": String,
-          "key": String
+          'value': String,
+          'key': String
         },
         {
-          "value": String,
-          "key": String
+          'value': String,
+          'key': String
         },
         {
-          "value": String,
-          "key": String
-        },
+          'value': String,
+          'key': String
+        }
       ],
-      "cluster_name": String
+      'cluster_name': String
     },
-    "services": [
+    'services': [
       {
-        "environment": [
+        'environment': [
           {
-            "value": String,
-            "key": String
+            'value': String,
+            'key': String
           }
         ],
-        "service_name": String,
-        "labels": [],
-        "deploy_labels": [
+        'service_name': String,
+        'labels': [],
+        'deploy_labels': [
           {
-            "label": String,
-            "value": String
+            'label': String,
+            'value': String
           },
           {
-            "label": String,
-            "value": String
+            'label': String,
+            'value': String
           },
           {
-            "label": String,
-            "value": String
+            'label': String,
+            'value': String
           },
           {
-            "label": String,
-            "value": String
+            'label': String,
+            'value': String
           },
           {
-            "label": String,
-            "value": String
+            'label': String,
+            'value': String
           },
           {
-            "label": String,
-            "value": String
+            'label': String,
+            'value': String
           }
         ],
-        "image": {
-          "static_version": String,
-          "image_name": String,
-          "semver_version": String
+        'image': {
+          'static_version': String,
+          'image_name': String,
+          'semver_version': String
         }
       }
     ]
