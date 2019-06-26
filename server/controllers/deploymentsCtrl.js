57c4ec7b09932c140e7543fa4b05b5fdd349ebe2
Patric Jansson <paddy@kth.se>
2019-06-26 16:15:58 +0200

    Channel id sucks...

--- server/controllers/deploymentsCtrl.js
+++ server/controllers/deploymentsCtrl.js
@@ -320,7 +320,7 @@ function* addLatestForApplicationName(request, response, next) {
           }.`
         );
         slack.sendMessage(
-          `<#C024BE7LR}|#${deployment.team}> service *${
+          `*${deployment.team}'s* service *${
             deployment.friendlyName
           }* is updated in production. - ${deployment.applicationUrl}.`
         );
