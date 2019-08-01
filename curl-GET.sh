71aef3a3685be487a11425a444a9dd5840652791
Patric Jansson <paddy@kth.se>
2019-08-01 14:52:12 +0200

    mor docs

--- curl-GET.sh
+++ curl-GET.sh
@@ -1,37 +1,11 @@
 #!/usr/bin/env bash
 
-# echo "--------------------------- ON PREM ---------------------------"
+echo "--------------------------- ON PREM ---------------------------"
 
-# curl  -s -S \
-#      --header "api_key: $API_KEY_READ" \
-#      --header "Content-Type: application/json" \
-#      "localhost:3001/api/pipeline/v1/latest/on-prem" | jq
-
-
-# echo ""
-# echo ""
-# echo "---------------------------- STAGE ----------------------------"
-
-# curl  -s -S \
-#      --header "api_key: $API_KEY_READ" \
-#      --header "Content-Type: application/json" \
-#      "localhost:3001/api/pipeline/v1/latest/stage" | jq
-
-# echo ""
-# echo ""
-# echo "---------------------------- ACTIVE ----------------------------"
-
-# curl  -s -S \
-#      --header "api_key: $API_KEY_READ" \
-#      --header "Content-Type: application/json" \
-#      "localhost:3001/api/pipeline/v1/latest/active" | jq
-
-# curl -s -S  \
-#      --header "api_key: $API_KEY_READ" \
-#      --header "Accept: application/json" \
-#      "https://api.kth.se/api/pipeline/v1/monitor/active/https%3A%2F%2Fapp.kth.se%2Fkth-azure-app%2F_monitor" | jq
-
-
+curl  -s -S \
+     --header "api_key: $API_KEY_READ" \
+     --header "Content-Type: application/json" \
+     "localhost:3001/api/pipeline/v1/latest/on-prem" | jq
 
 
 echo ""
@@ -39,7 +13,46 @@ echo ""
 echo "---------------------------- INTEGRAL ----------------------------"
 
 curl  -s -S \
+    --header "api_key: $API_KEY_READ" \
+    --header "Content-Type: application/json" \
+    "localhost:3001/api/pipeline/v1/latest/integral" | jq
+
+
+echo ""
+echo ""
+echo "---------------------------- STAGE ----------------------------"
+
+curl  -s -S \
      --header "api_key: $API_KEY_READ" \
      --header "Content-Type: application/json" \
-     "localhost:3001/api/pipeline/v1/latest/integral" | jq
+     "localhost:3001/api/pipeline/v1/latest/stage" | jq
+
+echo ""
+echo ""
+echo "---------------------------- ACTIVE ----------------------------"
+
+curl  -s -S \
+     --header "api_key: $API_KEY_READ" \
+     --header "Content-Type: application/json" \
+      "localhost:3001/api/pipeline/v1/latest/active" | jq
+
+echo ""
+echo ""
+echo "---------------------------- Get by absolute monitor url  ----------------------------"
+
+curl -s -S  \
+     --header "api_key: $API_KEY_READ" \
+     --header "Accept: application/json" \
+     "https://api.kth.se/api/pipeline/v1/monitor/active/https%3A%2F%2Fapp.kth.se%2Fkth-azure-app%2F_monitor" | jq
+
+echo ""
+echo ""
+echo "---------------------------- Search by starting path '/kth-azure-app/'  ----------------------------"
+
+curl -s -S  \
+     --header "api_key: $API_KEY_READ" \
+     --header "Accept: application/json" \
+     "https://api.kth.se/api/pipeline/v1/search/active/%2Fkth-azure-app%2F" | jq
+
+
 
