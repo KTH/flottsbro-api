006573ab12cc3acc9b924e40fa5cb327340c339e
Patric Jansson <paddy@kth.se>
2019-10-16 16:26:58 +0200

    Importance in logg

--- server/controllers/utils/slackUtils.js
+++ server/controllers/utils/slackUtils.js
@@ -7,14 +7,10 @@ const log = require("kth-node-log");
 function sendToDeploymentSlackChannel(deployment) {
   if (deploymentUtil.isProduction(deployment.cluster)) {
     log.info(
-      `Slack for all deployments in  (if configured) - ${
-        deployment.applicationName
-      }.`
+      `Slack for all deployments in  (if configured) - ${deployment.applicationName}.`
     );
 
-    let message = `*#${deployment.team}'s* service *${
-      deployment.friendlyName
-    }* is updated in production.`;
+    let message = `Importance: :importance-${deployment.importance}: ${deployment.importance} *#${deployment.team}'s* service *${deployment.friendlyName}* is updated in production.`;
 
     if (deployment.applicationUrl != null && deployment.applicationUrl != "") {
       message = `${message} - ${deployment.applicationUrl}`;
