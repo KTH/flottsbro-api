07d55facc0a519e932384561241a86de639de0fd
Patric Jansson <paddy@kth.se>
2019-11-14 16:30:23 +0100

    example

--- curl-DELETE.sh
+++ curl-DELETE.sh
@@ -5,4 +5,4 @@ curl -s -S  \
      -X DELETE \
      --header "api_key: $API_KEY_WRITE" \
      --header "Accept: application/json" \
-     "http://localhost:3001/api/pipeline/v1/active/innovation-web" | jq
+     "https://api.kth.se/api/pipeline/v1/on-prem/Social" | jq
