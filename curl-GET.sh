b34461a8d78ed3213683de30b2092e39eb56d78d
Patric Jansson <paddy@kth.se>
2020-03-15 20:38:19 +0100

    package update

--- curl-GET.sh
+++ curl-GET.sh
@@ -1,19 +1,22 @@
 #!/usr/bin/env bash
 
+#ENDPOINT='https://api.kth.se'
+ENDPOINT='http://localhost:3001'
+
 # curl -S  \
 #      --header "api_key: $API_KEY_READ" \
 #      --header "Accept: application/json" \
-#      "https://api.kth.se/api/pipeline/v1/search/active/%2Fkth-azure-app%2F_monitor" | jq
+#      "$ENDPOINT/api/pipeline/v1/search/active/%2Fkth-azure-app%2F_monitor" | jq
 
 # curl -s -S  \
 #      --header "api_key: $API_KEY_READ" \
 #      --header "Accept: application/json" \
-#      "https://api.kth.se/api/pipeline/v1/latest/active/innovation-api" | jq
+#      "$ENDPOINT/api/pipeline/v1/latest/active/innovation-api" | jq
 
 # curl -s -S  \
 #      --header "api_key: $API_KEY_READ" \
 #      --header "Accept: application/json" \
-#      "https://api.kth.se/api/pipeline/v1/monitor/active/https%3A%2F%2Fapp.kth.se%2Fkth-azure-app%2F_monitor" | jq
+#      "$ENDPOINT/api/pipeline/v1/monitor/active/https%3A%2F%2Fapp.kth.se%2Fkth-azure-app%2F_monitor" | jq
 
 
 # curl -s -S  \
@@ -24,4 +27,4 @@
 curl -s -S  \
      --header "api_key: $API_KEY_READ" \
      --header "Accept: application/json" \
-     "http://localhost:3001/api/pipeline/v1/latest/production/?importance=high" | jq
+     "$ENDPOINT/api/pipeline/v1/latest/production/?importance=high" | jq
