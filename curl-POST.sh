1fdf56483cbbdd4afdb51b6ab66d664246b72309
Patric Jansson <paddy@kth.se>
2020-03-19 12:05:10 +0100

    Updated docs

--- curl-POST.sh
+++ curl-POST.sh
@@ -1,31 +1,23 @@
 #!/usr/bin/env bash
 
-# curl --request POST --data \
-# '{
-#     "applicationName" : "social",
-#     "cluster" : "on-prem",
-#     "version" : "4.29.1-344",
-#     "importance" : "medium",
-#     "publicNameSwedish" : "Social",
-#     "publicNameEnglish" : "Social",
-#     "descriptionSwedish": "Course and programme webs and more",
-#     "descriptionEnglish": "Kurs och programwebbar",
-#     "applicationUrl" : "https://www.kth.se/social",
-#     "monitorPattern": "APPLICATION_STATUS: OK",
-#     "monitorUrl" : "https://www.kth.se/social/_monitor",
-#     "team" : "team-e-larande"
-# }' \
-# --silent \
-# --header "api_key: $API_KEY_WRITE"  \
-# --header "Content-Type: application/json" \
-# https://api.kth.se/api/pipeline/v1/latest/on-prem | jq
+#
+# Insert services manually into https://app.kth.se/pipeline/
+#
+# Configurations are documented here: https://gita.sys.kth.se/Infosys/cellus-registry/blob/master/HOW-TO-CONFIGURE.md
+#
 
+#
+# Where is the service run?
+#
+RUNS_WHERE='on-prem'
+# RUNS_WHERE='active'
+# RUNS_WHERE='saas'
 
 curl --request POST --data \
 '{
   "applicationName": "Annual Bibliometric Monitoring",
   "version": "1.0.0",
-  "cluster": "on-prem",
+  "cluster": "$RUNS_WHERE",
   "applicationUrl": "https://www.kth.se/abm",
   "monitorUrl": "https://www.kth.se/abm",
   "monitorPattern": "Since your browser does not support JavaScript",
@@ -41,4 +33,25 @@ curl --request POST --data \
 --silent \
 --header "api_key: $API_KEY_WRITE"  \
 --header "Content-Type: application/json" \
-https://api.kth.se/api/pipeline/v1/latest/on-prem
+https://api.kth.se/api/pipeline/v1/latest/$RUNS_WHERE
+
+
+# curl --request POST --data \
+# '{
+#     "applicationName" : "social",
+#     "cluster" : "$RUNS_WHERE",
+#     "version" : "4.29.1-344",
+#     "importance" : "medium",
+#     "publicNameSwedish" : "Social",
+#     "publicNameEnglish" : "Social",
+#     "descriptionSwedish": "Course and programme webs and more",
+#     "descriptionEnglish": "Kurs och programwebbar",
+#     "applicationUrl" : "https://www.kth.se/social",
+#     "monitorPattern": "APPLICATION_STATUS: OK",
+#     "monitorUrl" : "https://www.kth.se/social/_monitor",
+#     "team" : "team-e-larande"
+# }' \
+# --silent \
+# --header "api_key: $API_KEY_WRITE"  \
+# --header "Content-Type: application/json" \
+# https://api.kth.se/api/pipeline/v1/latest/$RUNS_WHERE 
