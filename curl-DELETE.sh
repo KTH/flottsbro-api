22d2a867266b9338fe55835cf10466d5360e3dc2
Patric Jansson <paddy@kth.se>
2020-05-01 23:15:35 +0200

    pkg update

--- curl-DELETE.sh
+++ curl-DELETE.sh
@@ -5,4 +5,5 @@ curl -s -S  \
      -X DELETE \
      --header "api_key: $API_KEY_WRITE" \
      --header "Accept: application/json" \
-     "https://api.kth.se/api/pipeline/v1/on-prem/cortina" | jq
+     "https://api.kth.se/api/pipeline/v1/active/office365optin" | jq
+
